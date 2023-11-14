f3f804355c737d8b639bac586ba53eff69a759a5
Alice Escalera <alice.escalera@gmail.com>
2023-11-14 18:56:43 +0100

    + add verification is number

--- exo/ex9.py
+++ exo/ex9.py
@@ -1,2 +1,15 @@
+def is_number(arg):
+    try:
+        int(arg)
+        is_number = True
+    except ValueError:
+        is_number = False
+    return is_number
+
 def calculate (numbers:list):
-    return [n*n for n in numbers if n>0]
+    if len(numbers) == 10:
+        return [n*n for n in numbers if is_number(n) and int(n) >0]
+    else:
+        return
+
+print(calculate([1,7,9,5,8,0,4,7,3,9]))
