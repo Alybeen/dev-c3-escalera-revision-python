1b170b0dfc43a7feb3b836d8e8e6b6c2e9f66513
Alice Escalera <alice.escalera@gmail.com>
2023-11-14 16:40:25 +0100

    + add division

--- ex3.py
+++ ex3.py
@@ -2,12 +2,22 @@ import re
 def multiplication(numA:int, numB:int):
     return (numA*numB)
 
+def division(numA:int, numB:int):
+    print(numA//numB)
+
 def calcul(operation: str):
-    result = 0
-    if operation.find("x".lower()):
-        result = multiplication(int(operation[operation.find("x") - 1]), int(operation[operation.find("x") + 1]))
+    m=0
+    a=0
+    s=0
+    d=0
+    if operation.find("x".lower()) !=-1:
+        m = multiplication(int(operation[operation.find("x") - 1]), int(operation[operation.find("x") + 1]))
+    if operation.find("/") != -1:
+        numA = int(operation[operation.find("/") - 1])
+        numB = int(operation[operation.find("/") + 1])
+        d = division(numA, numB)
+
 
 operation = input("Entrez l'opération voulue:")
 
-
-calcul(operation)
+print(calcul(operation))
