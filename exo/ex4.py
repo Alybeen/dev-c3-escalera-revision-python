31504b3200f8766cbbbeaa634ac1ce7475427758
Alice Escalera <alice.escalera@gmail.com>
2023-11-14 16:59:28 +0100

    + add ex4

---
+++ exo/ex4.py
@@ -0,0 +1,11 @@
+listing = [7, 9, 11, 7, 4, 0, 9, 5, 3, 8]
+if len(listing) > 0:
+    listing.sort()
+    print("le chiffre minimum est : ", listing[0])
+    print("le chiffre maximum est :", listing[len(listing) - 1])
+    res = 0
+    for i in listing:
+        res += int(i)
+    print("la moyenne est : ", res / len(listing))
+else:
+    print("pas de nombres")
